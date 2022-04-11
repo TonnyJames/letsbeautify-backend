2abbd7a22c0b1258ed79766281df73224272bca9
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2022-04-11 18:55:12 -0300

    Projeto integrador 2 iniciado

---
+++ src/main/java/com/projetointegrador/letsbeautfy/LetsBeautfyApplication.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.letsbeautfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsBeautfyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsBeautfyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetointegrador/letsbeautfy/LetsBeautfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.letsbeautfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsBeautfyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
