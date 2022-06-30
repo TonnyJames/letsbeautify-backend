97d47915072a927f00d21dc0b8466dffcf5ecbbd
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2022-06-30 20:57:31 -0300

    Liberação do cors

--- src/main/java/com/projetointegrador/letsbeautfy/config/CorsConfiguration.java
+++ src/main/java/com/projetointegrador/letsbeautfy/config/CorsConfiguration.java
@@ -9,6 +9,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://lets-beautify.vercel.app/").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
