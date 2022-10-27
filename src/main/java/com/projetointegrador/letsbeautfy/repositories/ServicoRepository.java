12fdca51f1e7e4796755c87e814092f087fb4aac
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2022-10-26 21:17:02 -0300

    criação da interface de ServicoRepository

---
+++ src/main/java/com/projetointegrador/letsbeautfy/repositories/ServicoRepository.java
@@ -0,0 +1,7 @@
+package com.projetointegrador.letsbeautfy.repositories;
+
+import com.projetointegrador.letsbeautfy.domain.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
