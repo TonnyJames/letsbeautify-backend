e1fcf9fb55228f059919421e8572e1d34204e306
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2022-10-28 16:58:21 -0300

    Desenvolvimento de ServicoRepository

--- src/main/java/com/projetointegrador/letsbeautfy/repositories/ServicoRepository.java
+++ src/main/java/com/projetointegrador/letsbeautfy/repositories/ServicoRepository.java
@@ -3,5 +3,10 @@ package com.projetointegrador.letsbeautfy.repositories;
 import com.projetointegrador.letsbeautfy.domain.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+
+    Optional<Servico> findByNrInsc(String nrInsc);
+    Optional<Servico> findByEmail(String email);
 }
