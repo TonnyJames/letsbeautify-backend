4bab9a6285978186b844b3e4aaef9e5cb86bb8cc
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2022-11-02 14:09:38 -0300

    criando o método findByAdmin para servicoResource

--- src/main/java/com/projetointegrador/letsbeautfy/repositories/ServicoRepository.java
+++ src/main/java/com/projetointegrador/letsbeautfy/repositories/ServicoRepository.java
@@ -3,10 +3,12 @@ package com.projetointegrador.letsbeautfy.repositories;
 import com.projetointegrador.letsbeautfy.domain.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ServicoRepository extends JpaRepository<Servico, Integer> {
 
+    List<Servico> findByAdmin(Integer AdminID);
     Optional<Servico> findByNrInsc(String nrInsc);
     Optional<Servico> findByEmail(String email);
 }
